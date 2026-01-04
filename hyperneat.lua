53b6455cfdaf719ffb07c17009c5d5ad8801f3a8
Shakhzod Ikromov <aabbcc.double@gmail.com>
2026-01-04 18:42:12 +0200

    populate substrate

--- hyperneat.lua
+++ hyperneat.lua
@@ -3,11 +3,45 @@ local neat_settings = {}
 
 local HYPERNEAT = {}
 
-function HYPERNEAT.create_substrate(settings)
-  settings.input_count
-  settings.output_count
+function HYPERNEAT.create_2dsubstrate(settings)
+  local genome = settings.genome or neat.create_genome({
+    input_count = 5,
+    output_count = 1
+  })
 
-  local substrate = { settings = settings }
+  settings.genome = genome
+
+  local substrate = {
+    settings = settings,
+    inputs = {},
+    outputs = {},
+    weights = {},
+  }
+
+  for y = 0, settings.input_res - 1 do
+    for x = 0, settings.input_res - 1 do
+      table.insert(substrate.inputs, {
+        x = (x / (settings.input_res - 1)) * 2 - 1,
+        y = (y / (settings.input_res - 1)) * 2 - 1
+      })
+    end
+  end
+
+  for i = 0, settings.output_count - 1 do
+    table.insert(substrate.outputs, {
+      x = (i / (settings.output_count - 1)) * 2 - 1,
+      y = 1.0
+    })
+  end
+
+  for i, n in ipairs(substrate.inputs) do
+    substrate.weights[i] = {}
+    for j, o in ipairs(substrate.outputs) do
+      -- input x, input y, output x, output y, bias
+      substrate.weights[i][j] = neat.evaluate(genome, { n.x, n.y, o.x, o.y, 1.0 })[1]
+    end
+  end
+
   return substrate
 end
 
