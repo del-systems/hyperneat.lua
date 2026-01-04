dfa5ad2b9e42d05e616178e30cdfad13e7a514d3
Shakhzod Ikromov <aabbcc.double@gmail.com>
2026-01-04 19:34:19 +0200

    evolving

--- hyperneat.lua
+++ hyperneat.lua
@@ -3,16 +3,26 @@ local neat_settings = {}
 
 local HYPERNEAT = {}
 
+function HYPERNEAT.refill_weights(substrate, settings)
+  for i, n in ipairs(substrate.inputs) do
+    substrate.weights[i] = {}
+    for j, o in ipairs(substrate.outputs) do
+      -- input x, input y, output x, output y, bias
+      substrate.weights[i][j] = neat.evaluate(substrate.genome, { n.x, n.y, o.x, o.y, 1.0 })[1]
+    end
+  end
+end
+
 function HYPERNEAT.create_2dsubstrate(settings)
   local genome = settings.genome or neat.create_genome({
     input_count = 5,
     output_count = 1
   })
 
-  settings.genome = genome
-
   local substrate = {
+    genome = genome,
     settings = settings,
+    fitness = 0,
     inputs = {},
     outputs = {},
     weights = {},
@@ -34,15 +44,52 @@ function HYPERNEAT.create_2dsubstrate(settings)
     })
   end
 
-  for i, n in ipairs(substrate.inputs) do
-    substrate.weights[i] = {}
-    for j, o in ipairs(substrate.outputs) do
-      -- input x, input y, output x, output y, bias
-      substrate.weights[i][j] = neat.evaluate(genome, { n.x, n.y, o.x, o.y, 1.0 })[1]
-    end
-  end
+  HYPERNEAT.refill_weights(substrate, settings)
 
   return substrate
+end
+
+
+function HYPERNEAT.evaluate(substrate, inputs, settings)
+  local sigmoid = neat.get_default_activation_by_name("sigmoid")
+  local results = {}
+
+  for j, output in ipairs(substrate.outputs) do
+    local sum = 0
+
+    for i, input in ipairs(substrate.inputs) do
+      sum = sum + (input * substrate.weights[i][j])
+    end
+
+    table.insert(results, sigmoid.fn(sum))
+  end
+
+  return results
+end
+
+function HYPERNEAT.evolve_population(population)
+  local genomes = {}
+  local next_gen = {}
+
+  for _, substrate in ipairs(population) do
+    table.insert(genomes, substrate.genome)
+    table.insert(next_gen, {
+      settings = substrate.settings,
+      inputs = substrate.inputs,
+      outputs = substrate.outputs,
+      weights = {}
+    })
+    substrate.genome.fitness = substrate.fitness
+  end
+
+  genomes = neat.evolve_population(genomes)
+  for i, substrate in ipairs(next_gen) do
+    substrate.fitness = 0
+    substrate.genome = genomes[i]
+    HYPERNEAT.refill_weights(substrate)
+  end
+
+  return next_gen
 end
 
 return HYPERNEAT
